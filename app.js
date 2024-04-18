646117986ef0151b2dba7b62d60a9fc2a175ed71
louisechan <louise.chan@101medialab.com>
2024-04-18 22:08:33 +0800

    Adding files

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+  // Shorthand for data: function() { return {} }
+  data() {
+    return {
+      url: "http://www.thenetninja.co.uk",
+      showBooks: true,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
+
+app.mount("#app");
